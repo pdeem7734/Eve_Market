a48371fa0f28aee9ab263f9ac8f66409f49ab777
Haxin <Haxin@10.129.240.212>
2014-09-03 17:02:46 -0600

    updated MySQL connection for the constructor to accept arguments

--- src/market/database/MySQLMarketConnection.java
+++ src/market/database/MySQLMarketConnection.java
@@ -6,16 +6,47 @@ public class MySQLMarketConnection implements SQL_Connection {
 	private Connection connection;
 	private Statement statement;
 		
-	//reutrns the market statement
+	//Returns the market statement
 	@Override
 	public Statement getMarketStatement() throws SQLException, ClassNotFoundException {
-		Class.forName("com.mysql.jdbc.Driver");
-		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/market_data?user=general");
 		statement = connection.createStatement();
 		return statement;
 	}
 	//
 	public MySQLMarketConnection() {
-		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/market_data?user=general");
+		} catch (Exception e) {
+			//open connection failed
+		}
+	}
+	public MySQLMarketConnection(String serverLocation, String userID, String password) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://" + serverLocation
+					+ "/market_data?user=" + userID + "&password=" + password);
+		} catch (Exception e) {
+			//open connection failed
+		}
+	}
+	public MySQLMarketConnection(String serverLocation, String userID) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://" + serverLocation
+					+ "/market_data?user=" + userID );
+		} catch (Exception e) {
+			//open connection failed
+		}
+	}
+	
+	public MySQLMarketConnection(String serverLocation) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://" + serverLocation
+					+ "/market_data?user=general");
+		} catch (Exception e) {
+			//open connection failed
+		}
 	}
 }

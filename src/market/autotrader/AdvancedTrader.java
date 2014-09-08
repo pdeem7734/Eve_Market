91eab2ed64272750726f1fe202dcccf48cfa1c58
Paul <pdeem_code@outlook.com>
2014-09-08 13:42:53 -0600

    updated advanced trader to have basic trend tracking.

--- src/market/autotrader/AdvancedTrader.java
+++ src/market/autotrader/AdvancedTrader.java
@@ -5,25 +5,32 @@ import java.math.*;
 
 import market.database.*;
 public class AdvancedTrader extends Trader {
-
+	static enum TrendDirection {UP, DOWN, STABLE};
+	private TrendDirection trendDirection;
 	public AdvancedTrader(MarketData marketData){
 		this.marketData = marketData;
 	}
 	@Override
 	public Trade[] suggestTrades() {
 		//we are going to start out with some basic trend analysis
-		Trade trade1 = new Trade(new Integer(34), "Trit");
+		Trade trade1 = new Trade(new Integer(623), "Trit");
 		marketData.loadCrestInfo(425, new Trade[] {trade1});
 		
 		ArrayList<Integer> positiveTrendLength = new ArrayList<Integer>();		
 		ArrayList<Integer> negativeTrendLength = new ArrayList<Integer>();
-		ArrayList<Integer> stableTrandLength = new ArrayList<Integer>();
+		ArrayList<Integer> stableTrendLength = new ArrayList<Integer>();
+		
+		ArrayList<TrendDirection> trendIndex = new ArrayList<TrendDirection>();
+		BigDecimal averagePriceDifference;
+		
 		
 		//we can only implement this like this because we are using a single itemID 
-		TreeMap<String, BigDecimal[]> historicalData = marketData.crestData.get(new Integer(34));
-		
-		
+		TreeMap<String, BigDecimal[]> historicalData = marketData.crestData.get(new Integer(623));
+				
 		String threeDaysAhead;
+		Integer trendLength = new Integer(1);
+		TrendDirection lastTrendDirection = TrendDirection.STABLE;
+		TrendDirection curentTrendDirection = TrendDirection.STABLE;
 		
 		
 		//this will iterate from the start date to the end date.
@@ -31,15 +38,59 @@ public class AdvancedTrader extends Trader {
 		for(String date : historicalData.keySet()) {
 			try {
 			//current solution is stupid but it works, so we will work with it. 
+			//TODO: change to a calendar based increment solution
 			threeDaysAhead = historicalData.higherKey(historicalData.higherKey(historicalData.higherKey(date)));
-
-			if (historicalData.get(threeDaysAhead)[4].compareTo(historicalData.get(date)[4]) > 0) {
-				System.out.print("UP");
-			} else  if (historicalData.get(threeDaysAhead)[4].compareTo(historicalData.get(date)[4].multiply(new BigDecimal(.98))) > 0) {
-				//if this is true it's with 2% south variance which i will call stable
+			
+			//gets the difference between date 3 days ahead and the curen't price
+			//Positive represents a rise in price
+			averagePriceDifference = historicalData.get(threeDaysAhead)[4].subtract(historicalData.get(date)[4]);
+			
+			//test to see if it is within 1% of the original price
+			if (averagePriceDifference.abs().compareTo(historicalData.get(date)[4].multiply(new BigDecimal(.01))) < 0) {
+				trendIndex.add(TrendDirection.STABLE);
 				System.out.print("STABLE");
-			} else {
+				
+				lastTrendDirection = curentTrendDirection;
+				curentTrendDirection = TrendDirection.STABLE;
+				
+				if (curentTrendDirection == lastTrendDirection) {
+					trendLength ++;
+					System.out.print(" " + trendLength);
+				} else {
+					trendLength = 1;
+					System.out.print(" " + trendLength);
+				}
+				
+				
+			} else  if (averagePriceDifference.compareTo(new BigDecimal(0)) > 0) { //checks the sign, if it's positive it's an up trend
+				trendIndex.add(TrendDirection.UP);
+				System.out.print("UP");
+				
+				lastTrendDirection = curentTrendDirection;
+				curentTrendDirection = TrendDirection.UP;
+				
+				if (curentTrendDirection == lastTrendDirection) {
+					trendLength ++;
+					System.out.print(" " + trendLength);
+				} else {
+					trendLength = 1;
+					System.out.print(" " + trendLength);
+				}
+				
+			} else { //if it has passed these points it's a down trend
+				trendIndex.add(TrendDirection.DOWN);
 				System.out.print("DOWN");
+				
+				lastTrendDirection = curentTrendDirection;
+				curentTrendDirection = TrendDirection.DOWN;
+				
+				if (curentTrendDirection == lastTrendDirection) {
+					trendLength ++;
+					System.out.print(" " + trendLength);
+				} else {
+					trendLength = 1;
+					System.out.print(" " + trendLength);
+				}
 			}
 			System.out.println(" : Start Date " + date);
 			

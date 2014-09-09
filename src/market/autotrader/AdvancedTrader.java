af7e89fe9ccad99e26869235dbdc87972ae42584
Paul <pdeem_code@outlook.com>
2014-09-09 07:11:20 -0600

    added a few logic bits to advnaced trader will complete today

--- src/market/autotrader/AdvancedTrader.java
+++ src/market/autotrader/AdvancedTrader.java
@@ -7,25 +7,46 @@ import market.database.*;
 public class AdvancedTrader extends Trader {
 	static enum TrendDirection {UP, DOWN, STABLE};
 	private TrendDirection trendDirection;
+	
+	ArrayList<Integer> positiveTrendLengths = new ArrayList<Integer>();		
+	ArrayList<Integer> negativeTrendLengths = new ArrayList<Integer>();
+	ArrayList<Integer> stableTrendLengths = new ArrayList<Integer>();
+	
+	
 	public AdvancedTrader(MarketData marketData){
 		this.marketData = marketData;
 	}
+	
+	private void addTrend(TrendDirection trendType, Integer length) {
+		switch(trendType) {
+		case UP:
+			positiveTrendLengths.add(length);
+			break;
+		case STABLE:
+			negativeTrendLengths.add(length);
+			break;
+		case DOWN:
+			stableTrendLengths.add(length);
+			break;
+		default:
+			throw new AssertionError("Unknown TrendType");
+				
+		}
+	}
+	
+	
 	@Override
 	public Trade[] suggestTrades() {
 		//we are going to start out with some basic trend analysis
-		Trade trade1 = new Trade(new Integer(623), "Trit");
+		Trade trade1 = new Trade(new Integer(29668), "PLEX");
 		marketData.loadCrestInfo(425, new Trade[] {trade1});
-		
-		ArrayList<Integer> positiveTrendLength = new ArrayList<Integer>();		
-		ArrayList<Integer> negativeTrendLength = new ArrayList<Integer>();
-		ArrayList<Integer> stableTrendLength = new ArrayList<Integer>();
 		
 		ArrayList<TrendDirection> trendIndex = new ArrayList<TrendDirection>();
 		BigDecimal averagePriceDifference;
 		
 		
 		//we can only implement this like this because we are using a single itemID 
-		TreeMap<String, BigDecimal[]> historicalData = marketData.crestData.get(new Integer(623));
+		TreeMap<String, BigDecimal[]> historicalData = marketData.crestData.get(new Integer(29668));
 				
 		String threeDaysAhead;
 		Integer trendLength = new Integer(1);
@@ -48,56 +69,61 @@ public class AdvancedTrader extends Trader {
 			//test to see if it is within 1% of the original price
 			if (averagePriceDifference.abs().compareTo(historicalData.get(date)[4].multiply(new BigDecimal(.01))) < 0) {
 				trendIndex.add(TrendDirection.STABLE);
-				System.out.print("STABLE");
+				
 				
 				lastTrendDirection = curentTrendDirection;
 				curentTrendDirection = TrendDirection.STABLE;
 				
 				if (curentTrendDirection == lastTrendDirection) {
 					trendLength ++;
-					System.out.print(" " + trendLength);
 				} else {
+					System.out.print(trendLength + " ");
+					System.out.print(lastTrendDirection);
+					addTrend(lastTrendDirection, trendLength);
 					trendLength = 1;
-					System.out.print(" " + trendLength);
+					System.out.println(" : Start Date " + date);
 				}
 				
 				
 			} else  if (averagePriceDifference.compareTo(new BigDecimal(0)) > 0) { //checks the sign, if it's positive it's an up trend
 				trendIndex.add(TrendDirection.UP);
-				System.out.print("UP");
 				
 				lastTrendDirection = curentTrendDirection;
 				curentTrendDirection = TrendDirection.UP;
 				
 				if (curentTrendDirection == lastTrendDirection) {
 					trendLength ++;
-					System.out.print(" " + trendLength);
 				} else {
+					System.out.print(trendLength + " ");
+					System.out.print(lastTrendDirection);
+					addTrend(lastTrendDirection, trendLength);
 					trendLength = 1;
-					System.out.print(" " + trendLength);
+					System.out.println(" : Start Date " + date);
 				}
 				
 			} else { //if it has passed these points it's a down trend
 				trendIndex.add(TrendDirection.DOWN);
-				System.out.print("DOWN");
 				
 				lastTrendDirection = curentTrendDirection;
 				curentTrendDirection = TrendDirection.DOWN;
 				
 				if (curentTrendDirection == lastTrendDirection) {
 					trendLength ++;
-					System.out.print(" " + trendLength);
 				} else {
+					System.out.print(trendLength + " ");
+					System.out.print(lastTrendDirection);
+					addTrend(lastTrendDirection, trendLength);
 					trendLength = 1;
-					System.out.print(" " + trendLength);
+					System.out.println(" : Start Date " + date);
 				}
 			}
-			System.out.println(" : Start Date " + date);
-			
 			} catch (NullPointerException e) {
 				//TODO: add logic so this step isn't required
 			}
 		}
+		System.out.println("Total Number of UP Trends     : " + positiveTrendLengths.size());
+		System.out.println("Total Number of STABLE Trends : " + stableTrendLengths.size());
+		System.out.println("Total Number of DOWN Trends   : " + negativeTrendLengths.size());
 		
 		return null;
 	}
